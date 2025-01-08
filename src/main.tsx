a85331dc06794bf8c7c21ef98f5d47051a625c29
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-08 16:00:05 +0530

    Prop Drilling - Crash Course

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Prop from "./Prop.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+createRoot(document.getElementById("prop-drilling")!).render(
+  <StrictMode>
+    <Prop />
+  </StrictMode>
+);
