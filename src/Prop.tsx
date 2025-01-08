093a5b6b0ca2429b9d867efe51c5ffaf3adfd786
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-08 16:53:33 +0530

    Prop-Drilling - Crash Course

--- src/Prop.tsx
+++ src/Prop.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Prop = () => {
-  return <div>Prop</div>;
+const Prop: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <Count count={count} />
+      <Button count={count} setCount={setCount} />
+    </div>
+  );
+};
+
+//we cannot directly add the arguments in the function in typescript we need to define it
+type CountProps = {
+  count: number;
+};
+
+const Count: React.FC<CountProps> = ({ count }) => {
+  return <div>{count}</div>;
+};
+
+type ButtonProps = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+const Button: React.FC<ButtonProps> = ({ count, setCount }) => {
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
 };
 
 export default Prop;
